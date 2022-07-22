600bdbf6f4fe9c6a43da9d30e29f72140772aac8
marlonveiga-santos <marlon_veiga@hotmail.com>
2022-07-22 18:20:32 -0300

    structure
    Add mockup frontend card structure.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
+                    <h2>Olá {{ Auth::user()->name }}</h2>
                     You're logged in!
                 </div>
             </div>
         </div>
+    </div>
+
+    <div class="">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-2 bg-white border-b border-gray-200">
+                    <h2>Your Polls:</h2>
+                </div>
+                @for ($i = 0; $i < 10; $i++) <div class="max-w-sm rounded overflow-hidden shadow-lg bg-slate-300 drop-shadow-md hover:drop-shadow-xl my-5">
+                    <div class="px-6 py-4">
+                        <div class="font-bold text-xl mb-2 dark:text-slate-700">Poll {{$i + 1}}</div>
+                        <p class="text-gray-700 text-base">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                        </p>
+                    </div>
+
+                    <div class="px-6 pt-4 pb-2">
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+                        <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+                    </div>
+            </div>
+            @endfor
+        </div>
+    </div>
     </div>
 </x-app-layout>
