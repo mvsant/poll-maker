a3c13f2b351102174757442b9d16a60f7598d445
marlonveiga-santos <marlon_veiga@hotmail.com>
2022-07-31 05:31:48 -0300

    refactor
    End new page frontend logoc.

--- resources/js/pollCreation.js
+++ resources/js/pollCreation.js
@@ -1,26 +1,53 @@
 const submitButton = document.getElementById("submitButton");
 const addAlternativeBtn = document.getElementById("addAlternativeBtn");
+const removeAlternativeBtn = document.getElementById("removeAlternativeBtn");
 const alternativesList = document.getElementById("alternativesList");
 
-const n = 3;
+
+function handleAlternative(altNumber){
+    return `
+    <div class="relative z-0 mb-6 group w-[40vw]">
+    <input type="text" name="alternative" id="alternative" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+    <label for="alternative" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Alternative ${altNumber}</label>
+    </div>` 
+}
+
 const alternative = `
 <div class="relative z-0 mb-6 group w-[40vw]">
 <input type="text" name="alternative" id="alternative" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-<label for="alternative" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Alternative</label>
+<label for="alternative" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Alternative ${alternativesList.childElementCount}</label>
 </div>`
-console.log(alternativesList.childElementCount);
+//console.log(alternativesList.childElementCount);
 
 
-function handleSubmitStatus(){}
+function handleSubmitStatus(size) {
+    if (size < 3) {
+        submitButton.disabled = true;
+        submitButton.style.opacity = "0.5";
+        submitButton.style.backgroundColor = "gray";
+
+    } else {
+        submitButton.disabled = false;
+        submitButton.style.opacity = "1";
+        submitButton.style.backgroundColor = "";
+    }
+    console.log(size);
+}
+
+handleSubmitStatus(alternativesList.childElementCount);
 
 addAlternativeBtn.addEventListener("click", (event) => {
+    let size = alternativesList.childElementCount;
     event.preventDefault();
-    alternativesList.insertAdjacentHTML('beforeend', alternative)
+    size++;
+    alternativesList.insertAdjacentHTML('beforeend', handleAlternative(size))
+    handleSubmitStatus(size);
 })
 
-if (n < 3) {
-    submitButton.disabled = true;
-    submitButton.style.opacity = "0.5";
-    submitButton.style.backgroundColor = "gray";
-
-}
+removeAlternativeBtn.addEventListener("click", (event) => {
+    let size = alternativesList.childElementCount
+    event.preventDefault();
+    size--;
+    alternativesList.removeChild(alternativesList.lastElementChild);
+    handleSubmitStatus(size);
+})
--- resources/views/polls/new.blade.php
+++ resources/views/polls/new.blade.php
@@ -17,11 +17,14 @@
                         <p class="dark:text-white mb-5">Please insert at least 3 alternatives</p>
                         <div id="alternativesList">
                             <div class="relative z-0 mb-6 group w-[40vw]">
-                                <input type="text" name="alternative" id="alternative" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <input type="text" name="alternative" id="alternative-1" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
                                 <label for="alternative" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Alternative</label>
                             </div>
                         </div>
+                        <div class="flex flex-row-reverse justify-between">
                         <button id="addAlternativeBtn" class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-600 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring ring-green-300 disabled:opacity-25 transition ease-in-out duration-150">+1</button>
+                            <button id="removeAlternativeBtn" class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-600 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150">-1</button>
+                        </div>
                         <br>
 
                         <button type="submit" id="submitButton" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
