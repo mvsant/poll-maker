83dace50b647ce558bbd12591b6be194adc5b7d1
marlonveiga-santos <marlon_veiga@hotmail.com>
2022-08-01 04:28:48 -0300

    refactor
    Minor frontend fixes.

--- resources/views/polls/new.blade.php
+++ resources/views/polls/new.blade.php
@@ -22,12 +22,13 @@
                             </div>
                         </div>
                         <div class="flex flex-row-reverse justify-between">
-                        <button id="addAlternativeBtn" class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-600 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring ring-green-300 disabled:opacity-25 transition ease-in-out duration-150">+1</button>
+                            <button id="addAlternativeBtn" class="inline-flex items-center px-4 py-2 bg-green-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-green-600 active:bg-green-900 focus:outline-none focus:border-green-900 focus:ring ring-green-300 disabled:opacity-25 transition ease-in-out duration-150">+1</button>
                             <button id="removeAlternativeBtn" class="inline-flex items-center px-4 py-2 bg-red-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-600 active:bg-red-900 focus:outline-none focus:border-red-900 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150">-1</button>
                         </div>
                         <br>
-
-                        <button type="submit" id="submitButton" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        <div class="flex items-center mx-auto content-center">
+                            <button type="submit" id="submitButton" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-[80%] mx-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                        </div>
                     </form>
                 </div>
             </div>
