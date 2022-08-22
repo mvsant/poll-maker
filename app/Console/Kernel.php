b7ca7ae9d9a99b59f4fdc9e2603052faec09b56e
marlonveiga-santos <marlon_veiga@hotmail.com>
2022-08-22 05:29:16 -0300

    feature
    Add Cron structure (not implemented)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Models\Poll;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -15,7 +16,17 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+
+/* 
+            Setup a daily cron Job on your server to run this
+        $schedule->call(function () {
+            $now = date('Y-m-d h:m:i');
+            Poll::where(['start_date', '>', $now], ['end_date', '<', $now])->update([
+                'is_open' => false,
+            ]);
+        })->daily();
+ */
+
     }
 
     /**
@@ -25,7 +36,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
